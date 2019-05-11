ce82e73248c58bc0643056740393767fc48639e4
Genevieve DePriest <gdpparton@gmail.com>
2019-05-10 23:44:49 -0600

    made images img-thumbnails

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// Global Variables - wins, losses, target score, your score
+var wins = 0;
+var losses = 0;
+var target = 0;
+var yourNum = 0;
+
+var poop1 = $("#poop1");
+var poop2 = $("#poop2");
+var poop3 = $("#poop3");
+var poop4 = $("#poop4");
+
+$("document").ready(function() {
+    console.log("ready");
+});
+
+//var score  = yourScore + crystalValue
+
+
+//array of crystals to loop through and 
+
+
+//Random Number Function
+        //assign a value to teach crystal
+
+//click event function
+
+
+//start/reset
+    //
+
+// clear variables
+
+
+
+//DOM manipulation methods
+
+
+//  Lindsey - dynamically create crystals on the page.  We might create an object to hold each crystal.
+
